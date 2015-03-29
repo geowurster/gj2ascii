ffc213e6870043ee1160a2422332b99355927b7e
Kevin Wurster <wursterk@gmail.com>
2015-03-28 22:17:26 -0400

    Debugging an issue that only sometimes appears at Travis.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -173,27 +173,19 @@ class TestCli(unittest.TestCase):
         ])
         self.assertEqual(result.exit_code, 0)
 
-        # Different versions of python generate a list of layers for %all in a different order, so the test
-        # has to normalize this difference and modify the command output.
+        # fio.listlayers() does not produce a list with the same order every time on every platform so some
+        # adjustments have to be made
         output = result.output
-        if output.strip()[0] != '1':
+        # output.replace('0', 'x')
+        # output.replace('1', 'x')
+        if output.strip()[0] == '1':
             output.replace('0', 'z')
             output.replace('z', '1')
             output.replace('1', 'o')
             output.replace('o', '0')
-        import fiona as fio
-        print(fio.listlayers(MULTILAYER_FILE))
-        # print("")
-        # print("-=-=-=-=-=-=-=-=-")
-        # print(repr(output.strip()))
-        # print("-=-=-=-=-=-=-=-=-")
-        # print(repr(EXPECTED_STACK_PERCENT_ALL.strip()))
-        # print("-=-=-=-=-=-=-=-=-")
-        # print(output.strip())
-        # print("-=-=-=-=-=-=-=-=-")
-        # print(EXPECTED_STACK_PERCENT_ALL.strip())
-        # print("-=-=-=-=-=-=-=-=-")
         self.assertTrue(compare_ascii(output.strip(), EXPECTED_STACK_PERCENT_ALL.strip()))
+        self.assertTrue('0' in result.output)
+        self.assertTrue('1' in result.output)
 
     def test_render_one_layer_too_many_args(self):
         result = self.runner.invoke(cli.main, [
